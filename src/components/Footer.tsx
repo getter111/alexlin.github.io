326359b9ced5b445fc407c8e36121de2ac223297
Alex <ayl@g.clemson.edu>
2025-09-07 17:15:09 -0400

    fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
         <footer className="py-6 text-center bg-slate-800 text-slate-400 text-sm underline">
-            <a href="https://github.com/getter111" className="hover:text-teal-400"> Repository for this website </a> 
+            <a href="https://github.com/getter111/alexlin.github.io" className="hover:text-teal-400"> Repository for this website </a> 
         </footer>
     )
 }
